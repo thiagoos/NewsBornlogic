e4846d4f5dfae5830478139770f80a1757800d57
Thiago <thiagoos@hotmail.com>
2022-03-22 07:41:41 -0300

    Initial Commit

---
+++ NewsBornlogic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsBornlogic
+//
+//  Created by Thiago Soares on 22/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
